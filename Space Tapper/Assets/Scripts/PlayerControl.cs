b9be781c77c959096fdbda78c45af5cabf378f82
SkipeDAG <SkiprDAG@yandex.ru>
2021-10-15 17:59:01 +0800

    player movement
    добавил папку спрайты и скрипты

---
+++ Space Tapper/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,138 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+
+public class PlayerControl : MonoBehaviour
+{
+
+	public enum ProjectAxis { onlyX = 0, xAndY = 1 };
+	public ProjectAxis projectAxis = ProjectAxis.onlyX;
+	public float speed = 150;
+	public float addForce = 7;
+	private KeyCode leftButton = KeyCode.A;
+	private KeyCode rightButton = KeyCode.D;
+	//private KeyCode upButton = KeyCode.W;
+	//private KeyCode downButton = KeyCode.S;
+	private KeyCode addForceButton = KeyCode.Space;
+	public bool isFacingRight = true;
+	private Vector3 direction;
+	private float vertical;
+	private float horizontal;
+	private Rigidbody2D body;
+	private bool jump;
+
+	void Start()
+	{
+		body = GetComponent<Rigidbody2D>();
+		body.fixedAngle = true;
+
+		if (projectAxis == ProjectAxis.xAndY)
+		{
+			body.gravityScale = 0;
+			body.drag = 10;
+		}
+	}
+
+	void OnCollisionStay2D(Collision2D coll)
+	{
+		if (coll.transform.tag == "Ground")
+		{
+			body.drag = 10;
+			jump = true;
+		}
+	}
+
+	void OnCollisionExit2D(Collision2D coll)
+	{
+		if (coll.transform.tag == "Ground")
+		{
+			body.drag = 0;
+			jump = false;
+		}
+	}
+
+	void FixedUpdate()
+	{
+		body.AddForce(direction * body.mass * speed);
+
+		if (Mathf.Abs(body.velocity.x) > speed / 100f)
+		{
+			body.velocity = new Vector2(Mathf.Sign(body.velocity.x) * speed / 100f, body.velocity.y);
+		}
+
+		if (projectAxis == ProjectAxis.xAndY)
+		{
+			if (Mathf.Abs(body.velocity.y) > speed / 100f)
+			{
+				body.velocity = new Vector2(body.velocity.x, Mathf.Sign(body.velocity.y) * speed / 100f);
+			}
+		}
+		else
+		{
+			if (Input.GetKey(addForceButton) && jump)
+			{
+				body.velocity = new Vector2(0, addForce);
+			}
+		}
+	}
+
+	void Flip()
+	{
+		if (projectAxis == ProjectAxis.onlyX)
+		{
+			isFacingRight = !isFacingRight;
+			Vector3 theScale = transform.localScale;
+			theScale.x *= -1;
+			transform.localScale = theScale;
+		}
+	}
+
+	void Update()
+	{
+
+		//if (Input.GetKey(upButton))
+		//{
+		//	vertical = 1;
+		//}
+		//else if (Input.GetKey(downButton))
+		//{
+		//	vertical = -1;
+		//}
+		//else
+		//{
+		//	vertical = 0;
+		//}
+
+		if (Input.GetKey(leftButton))
+		{
+			horizontal = -1;
+		}
+		else if (Input.GetKey(rightButton))
+		{
+			horizontal = 1;
+		}
+		else
+		{
+			horizontal = 0;
+		}
+
+		if (projectAxis == ProjectAxis.onlyX)
+		{
+			direction = new Vector2(horizontal, 0);
+		}
+		else
+		{
+			if (Input.GetKeyDown(addForceButton))
+			{
+				speed += addForce;
+			}
+			else if (Input.GetKeyUp(addForceButton))
+			{
+				speed -= addForce;
+			}
+			direction = new Vector2(horizontal, vertical);
+		}
+
+		if (horizontal > 0 && !isFacingRight) Flip(); else if (horizontal < 0 && isFacingRight) Flip();
+	}
+}
