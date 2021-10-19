682578a46dcb97fe1cf9b4807afc6faef6330165
SkipeDAG <SkiprDAG@yandex.ru>
2021-10-19 20:29:39 +0800

    чуть чуть поменял
    удалил не нужная папку
    кинул пулю в префаб

--- Space Tapper/Assets/Scripts/FireScript.cs
+++ Space Tapper/Assets/Scripts/FireScript.cs
@@ -32,9 +32,9 @@ public class FireScript : MonoBehaviour
 			Rigidbody2D clone = Instantiate(bullet, gunPoint.position, Quaternion.identity) as Rigidbody2D;
 			clone.velocity = transform.TransformDirection(gunPoint.right * speed);
             print(clone.velocity);
-            if (clone.velocity.x<0) {
+            if (clone.velocity.x < 0) {
                 Vector3 scale = clone.transform.localScale;
-                clone.transform.localScale = new Vector3(-1*scale.x, scale.y, scale.z);
+                clone.transform.localScale = new Vector3(-1 * scale.x, scale.y, scale.z);
             }
 			clone.transform.right = gunPoint.right;
 		}
